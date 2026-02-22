373eddc7558247005fc4f3d12aacd90a43fea039
Krekerdgek <dimos-79@mail.ru>
2026-02-23 00:07:54 +0300

    Create __init__.py

---
+++ bot/services/__init__.py
@@ -0,0 +1,6 @@
+# Пакет сервисов
+from bot.services.card_generator import BusinessCardGenerator
+from bot.services.qr_service import QRService
+from bot.services.proxy_rotator import ProxyRotator
+from bot.services.backup import BackupService, start_backup_scheduler
+from bot.services.stats import StatsService
